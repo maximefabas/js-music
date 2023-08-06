7a3172a9a6e544a209fee89a145c549abdd18959
Maxime Fabas <maxime.fabas@pm.me>
2023-08-06 23:23:56 +0200

    -

--- src/index.ts
+++ src/index.ts
@@ -106,99 +106,63 @@ export type SimpleIntervalName = string
 
 export function simpleIntervalValueToName (value: SimpleIntervalValue): SimpleIntervalName {
   const { simpleIntervalClass, alteration } = value
+  const modulatedSimpleIntervalClass = absoluteModulo(simpleIntervalClass, 7)
   const alterationName = alterationValueToName(alteration)
   let simpleIntervalClassName: string
-  if (simpleIntervalClass > 0) { simpleIntervalClassName = `${simpleIntervalClass + 1}` }
-  else if (simpleIntervalClass === 0) { simpleIntervalClassName = '1' }
-  else { simpleIntervalClassName = `${-1 * simpleIntervalClass + 1}` }
+  if (modulatedSimpleIntervalClass > 0) { simpleIntervalClassName = `${modulatedSimpleIntervalClass + 1}` }
+  else if (modulatedSimpleIntervalClass === 0) { simpleIntervalClassName = '1' }
+  else { simpleIntervalClassName = `${-1 * modulatedSimpleIntervalClass + 1}` }
   return `${alterationName}${simpleIntervalClassName}`
 }
 
 export function simpleIntervalNameToValue (name: SimpleIntervalName): SimpleIntervalValue | undefined {
   const nameChars = name.split('')
   const simpleIntervalClassNameChars = nameChars.filter(char => char === '-' || !Number.isNaN(parseInt(char)))
-  const parsedSimpleIntervalClassValue = parseInt(simpleIntervalClassNameChars.join(''))
+  let parsedSimpleIntervalClassValue = parseInt(simpleIntervalClassNameChars.join(''))
   if (!Number.isInteger(parsedSimpleIntervalClassValue)) return undefined
-  let simpleIntervalClass: number
-  if (parsedSimpleIntervalClassValue > 1) { simpleIntervalClass = parsedSimpleIntervalClassValue - 1 }
-  else if (parsedSimpleIntervalClassValue < 1) { simpleIntervalClass = -1 * parsedSimpleIntervalClassValue - 1 }
-  else { simpleIntervalClass = 0 }
+  if (parsedSimpleIntervalClassValue >= 1) parsedSimpleIntervalClassValue -= 1
+  else if (parsedSimpleIntervalClassValue <= -1) parsedSimpleIntervalClassValue += 1
+  const modulatedSimpleIntervalClass = absoluteModulo(parsedSimpleIntervalClassValue, 7)  
+  const simpleIntervalClass = Math.abs(modulatedSimpleIntervalClass > 0
+    ? modulatedSimpleIntervalClass
+    : -1 * modulatedSimpleIntervalClass
+  )
   const alteration = alterationNameToValue(name)
   return { simpleIntervalClass, alteration }
 }
 
-export function simpleIntervalClassInvert (simpleIntervalClass: SimpleIntervalClass): SimpleIntervalClass {
-  console.log('simpleIntervalClassInvert - input', simpleIntervalClass)
-  console.log('simpleIntervalClassInvert - return', 7 - simpleIntervalClass)
-  return 7 - simpleIntervalClass
-}
-
-export function simpleIntervalClassToSemitones (simpleIntervalClass: SimpleIntervalClass): number | undefined {
-  console.log('simpleIntervalClassToSemitones - input', simpleIntervalClass)
-  const semitonesList = [0, 2, 4, 5, 7, 9, 11]
-  const loops = Math.floor(simpleIntervalClass / 7)
-  console.log('simpleIntervalClassToSemitones - loops', loops)
-  const absSimpleIntervalClass = absoluteModulo(simpleIntervalClass, 7)
-  console.log('simpleIntervalClassToSemitones - absSimpleIntervalClass', absSimpleIntervalClass)
-  const absSemitones = semitonesList.at(absSimpleIntervalClass)
-  console.log('simpleIntervalClassToSemitones - absSemitones', absSemitones)
-  if (absSemitones === undefined) return undefined
-  console.log('simpleIntervalClassToSemitones - return', 12 * loops + absSemitones)
-  return 12 * loops + absSemitones
-}
-
-export function simpleIntervalToSemitones (value: SimpleIntervalValue): number | undefined {
-  console.log('simpleIntervalToSemitones - input', value)
-  const { simpleIntervalClass, alteration } = value
-  const simpleIntervalClassAsSemitones = simpleIntervalClassToSemitones(simpleIntervalClass)
-  console.log('simpleIntervalToSemitones - simpleIntervalClassAsSemitones', simpleIntervalClassAsSemitones)
-  if (simpleIntervalClassAsSemitones === undefined) return undefined
-  console.log('simpleIntervalToSemitones - return', simpleIntervalClassAsSemitones + alteration)
-  return simpleIntervalClassAsSemitones + alteration
-}
-
-export function simpleIntervalInvert (value: SimpleIntervalValue): SimpleIntervalValue | undefined {
-  console.log('simpleIntervalInvert - input', value)
-  const inputAsSemitones = simpleIntervalToSemitones(value)
-  console.log('simpleIntervalInvert - inputAsSemitones', inputAsSemitones)
-  if (inputAsSemitones === undefined) return undefined
-  const outputAsSemitones = 12 - inputAsSemitones
-  console.log('simpleIntervalInvert - outputAsSemitones', outputAsSemitones)
-  const { simpleIntervalClass } = value
-  const invertedSimpleIntervalClass = simpleIntervalClassInvert(simpleIntervalClass)
-  console.log('simpleIntervalInvert - invertedSimpleIntervalClass', invertedSimpleIntervalClass)
-  const invertedSimpleIntervalClassAsSemitones = simpleIntervalClassToSemitones(invertedSimpleIntervalClass)
-  console.log('simpleIntervalInvert - invertedSimpleIntervalClassAsSemitones', invertedSimpleIntervalClassAsSemitones)
-  if (invertedSimpleIntervalClassAsSemitones === undefined) return undefined
-  const invertedAlteration = outputAsSemitones - invertedSimpleIntervalClassAsSemitones
-  console.log('simpleIntervalInvert - invertedAlteration', invertedAlteration)
-  console.log('simpleIntervalInvert - return', {
-    simpleIntervalClass: invertedSimpleIntervalClass,
-    alteration: invertedAlteration
-  })
+export function simpleIntervalInvert (value: SimpleIntervalValue): SimpleIntervalValue {
+  const { simpleIntervalClass: _simpleIntervalClass, alteration } = value
+  const simpleIntervalClass = absoluteModulo(_simpleIntervalClass, 7)
+  const invertedSimpleIntervalClass = absoluteModulo(7 - simpleIntervalClass, 7)
+  const semitoneValuesArr = [0, 2, 4, 5, 7, 9, 11]
+  const simpleIntervalClassSemitoneValue = semitoneValuesArr[simpleIntervalClass]
+  const invertedSimpleIntervalClassSemitoneValue = semitoneValuesArr[invertedSimpleIntervalClass]
+  const modulateedSemitonesSum = absoluteModulo(simpleIntervalClassSemitoneValue + invertedSimpleIntervalClassSemitoneValue, 12)
   return {
     simpleIntervalClass: invertedSimpleIntervalClass,
-    alteration: invertedAlteration
+    alteration: - 1 * modulateedSemitonesSum - alteration
   }
 }
 
-console.log(simpleIntervalValueToName(
-  simpleIntervalInvert(
-    simpleIntervalNameToValue('9') as any
-  ) as any
+export function simpleIntervalFromPitchClasses (
+  pitchClassA: PitchClassValue,
+  pitchClassB: PitchClassValue): SimpleIntervalValue {
+  const { pitchClassLetter: _pitchClassLetterA, alteration: alterationA } = pitchClassA
+  const { pitchClassLetter: _pitchClassLetterB, alteration: alterationB } = pitchClassB
+  const pitchClassLetterA = absoluteModulo(_pitchClassLetterA, 7)
+  const pitchClassLetterB = absoluteModulo(_pitchClassLetterB, 7)
+  const simpleIntervalClass = absoluteModulo(pitchClassLetterB - pitchClassLetterA, 7)
+  const semitoneValuesArr = [0, 2, 4, 5, 7, 9, 11]
+  const pitchClassLetterAAsSemitones = semitoneValuesArr[pitchClassLetterA]
+  const pitchClassLetterBAsSemitones = semitoneValuesArr[pitchClassLetterB]
+  return { simpleIntervalClass, alteration: 0 }
+}
+
+console.log(simpleIntervalFromPitchClasses(
+  pitchClassNameToValue('#f') as PitchClassValue,
+  pitchClassNameToValue('c') as PitchClassValue
 ))
-
-// export function simpleIntervalValueAsSemitones (value: SimpleIntervalValue): number {
-//   const { simpleIntervalClass, alteration } = value
-//   const simpleIntervalValuesArr = [0, 2, 4, 5, 7, 9, 11]
-//   const octaves = Math.ceil(simpleIntervalClass / 7)
-//   const octavesAsSemitones = 12 * octaves
-//   const modulatedSimpleInterval = absoluteModulo(simpleIntervalClass, 7)
-
-//   return simpleIntervalClass + alteration
-// }
-
-// [WIP] from semitones ? What to do with octaves ? direct convert octave to semitones, or go through simple interval or interval ?
 
 /* Interval */
 
